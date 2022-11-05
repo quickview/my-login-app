ba586b74878baca79f2581cad4cfc1d2a85a8e3b
Myung-Hyun <quickview12@gmail.com>
2022-11-05 17:00:54 +0900

    initialize firebase

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBNbmRz-o5_I3QaCVTP06zYqsQEfNUQ0H8",
+    authDomain: "my-login-3884d.firebaseapp.com",
+    projectId: "my-login-3884d",
+    storageBucket: "my-login-3884d.appspot.com",
+    messagingSenderId: "1071984280896",
+    appId: "1:1071984280896:web:f43721d70a19c4389aa6a5",
+    measurementId: "G-0EX1JQXD18"
+  };
+  
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {
+  FirebaseApi,
+  FirebaseContext,
+} from "./Firebase";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,7 +16,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <FirebaseContext.Provider value={new FirebaseApi()}>
+          <App/>
+        </FirebaseContext.Provider>
     </Provider>
   </React.StrictMode>
 );
